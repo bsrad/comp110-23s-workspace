52a078b56378b84cf75c3e46fd7c30fe26e035b0
Brayden Radhuber <bsrad@ad.unc.edu>
2023-03-05 20:39:43 -0500

    sum cq

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -2,55 +2,55 @@
 
 __author__ = "730487901"
 
-def all(num: list[int], all_int: int) -> bool:
+
+def all(nums: list[int], all_int: int) -> bool: 
+    """Indicate whether or not all ints in list are the same as given int."""
     x: int = 0
     playing = True
-    while x < len(num) and playing is True:
-        if all_int == num[x]:
+    if len(nums) == 0:
+        # no empty lists
+        playing = False
+    while x < len(nums) and playing is True:
+        if all_int == nums[x]:
             x += 1
+            # incrementing x to check each index of the list
         else: 
             playing = False
+            # false if they do not match
             x += 1
     return playing
 
-def max(input: list[int]) -> int:
+
+def max(input: list[int]) -> int: 
+    """Find the max int in a list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
-    y: int = 0
-    z: int = 0
+    check_idx: int = 0
+    look_idx: int = 0
     playing2 = True
-    while y < len(input) and z < len(input) and playing2 is True:
-        if input[y] > input[z]:
-            z += 1
+    while check_idx < len(input) and look_idx < len(input) and playing2 is True:
+        # variables must be smaller than len of list
+        if input[check_idx] > input[look_idx]:
+            look_idx += 1
+            # incrementing to isolate largest number
         else:
-            y += 1
-    return input[z]
+            check_idx += 1
+            # number is smaller, move on to next index
+    return input[look_idx]
 
-def is_equal(equal1: list[int], equal2: list[int]) -> bool:
-    xs: int = 0
+
+def is_equal(equal1: list[int], equal2: list[int]) -> bool: 
+    """Check to see if two lists are exactly equal."""
+    idx_check: int = 0
     playing3 = True
-    while xs < len(equal1) and xs < len(equal2) and playing3 is True:
-        if equal1[xs] == equal2[xs]:
-            xs += 1
+    if len(equal1) != len(equal2):
+        # lists must be equal in length
+        playing3 = False
+    while idx_check < len(equal1) and idx_check < len(equal2) and playing3 is True:
+        # variables must be smaller than length of list
+        if equal1[idx_check] == equal2[idx_check]:
+            # each list must be equal at every index, variable can increment the same for both
+            idx_check += 1
         else: 
             playing3 = False
     return playing3
-
-       
-        
-
-
-            
-        
-
-
-
-        
-
-       
-        
-            
-    
-
-
-
--- lessons/sum.py
+++ lessons/sum.py
@@ -3,9 +3,6 @@
 def sum(xs: list[float]) -> float:
     """return sum of all elements in xs"""
     sum_total: float = 0.0
-    idx: int = 0
-    while idx <= len(xs) - 1:
-        sum_total += xs[idx]
-        idx += 1
+    for item in xs:
+        sum_total += item
     return sum_total
-    
