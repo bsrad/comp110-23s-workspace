28ba52982c792b34ee5761e00c313cc3e9815557
Brayden Radhuber <bsrad@ad.unc.edu>
2023-04-26 12:23:29 -0400

    pre simpy

--- exercises/ex09/fish.py
+++ exercises/ex09/fish.py
@@ -1,9 +1,17 @@
-"""File to define Fish class"""
+"""File to define Fish class."""
+
 
 class Fish:
-    
+    """Fish."""
+
+    age: int
+
     def __init__(self):
+        """Init."""
+        self.age = 0
         return None
     
     def one_day(self):
+        """One day."""
+        self.age += 1
         return None
--- exercises/ex09/river.py
+++ exercises/ex09/river.py
@@ -1,12 +1,20 @@
-"""File to define River class"""
+"""File to define River class."""
+
+__author__ = "730487901"
 
 from exercises.ex09.fish import Fish
 from exercises.ex09.bear import Bear
 
+
 class River:
-    
-    def __init__(self, num_fish: int, num_bears:int):
-        """New River with num_fish Fish and num_bears Bears"""
+    """River."""
+
+    day: int
+    bears: list
+    fish: list
+
+    def __init__(self, num_fish: int, num_bears: int):
+        """New River with num_fish Fish and num_bears Bears."""
         self.day: int = 0
         self.fish: list[Fish] = []
         self.bears: list[Bear] = []
@@ -17,25 +25,68 @@ class River:
             self.bears.append(Bear())
 
     def check_ages(self):
+        """Check ages."""
+        survived_fish: list[str] = []
+        survived_bear: list[str] = []
+        for fish in self.fish:
+            if fish.age <= 3:
+                survived_fish.append(fish)
+        self.fish = survived_fish
+        for bear in self.bears:
+            if bear.age <= 5:
+                survived_bear.append(bear)
+        self.bear = survived_bear
         return None
 
     def bears_eating(self):
+        """Bear eating."""
+        for bear in self.bears:
+            if len(self.fish) >= 5:
+                bear.eat(3)
+                self.remove_fish(3)
         return None
     
     def check_hunger(self):
+        """Check hunger."""
+        surviving_bears: list = []
+        for bear in self.bears:
+            if bear.hunger_score >= 0:
+                surviving_bears.append(bear)
+        self.bears = surviving_bears
         return None
         
     def repopulate_fish(self):
+        """Repopulate fish."""
+        for x in self.fish:
+            x: int = (len(self.fish) // 2) * 4
+            self.fish.append(x)
         return None
-    
+
     def repopulate_bears(self):
+        """Repopulate bears."""
+        x: int = len(self.bears) // 2
+        self.bears.append(x)
         return None
-    
+
+    def remove_fish(self, amount: int):
+        """Remove fish."""
+        x: int = 0
+        while x < amount:
+            self.fish.pop(x)
+            x += 1
+
     def view_river(self):
+        """View river."""
+        x: int = self.day
+        y: int = len(self.fish)
+        z: int = len(self.bears)
+        print(f"~~~ Day: {x} ~~~")
+        print(f"Fish population: {y}")
+        print(f"Bear population: {z}")
         return None
             
     def one_river_day(self):
-        """Simulate one day of life in the river"""
+        """Simulate one day of life in the river."""
         # Increase day by 1
         self.day += 1
         # Simulate one day for all Bears
@@ -56,4 +107,10 @@ class River:
         self.repopulate_bears()
         # Visualize River
         self.view_river()
-            
+
+    def one_river_week(self):
+        """One river week."""
+        x: int = 0
+        while x < 7:
+            self.one_river_day()
+            x += 1
